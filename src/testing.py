aca64584acccdf725b4a30ea38897fee4af2d5c7
Frank Milthaler <f.milthaler@gmail.com>
2018-12-07 20:26:25 +0000

    Cleaning up and playing around with the created data structures in the ipython notebook

--- src/testing.py
+++ src/testing.py
@@ -26,6 +26,10 @@ import portfolio
 
 # <codecell>
 
+import mytools as mt
+
+# <codecell>
+
 # to plot within the notebook:
 %pylab inline
 #pylab
@@ -109,97 +113,34 @@ def SharpeRatio(x):
 
 # <codecell>
 
-#for row in df_pf.itertuples(index=True, name='Pandas'):
-columns = df_pf.columns
-funds = {}
-for idx, row in df_pf.iterrows():
-    #print(row.Vintage, row.FMV)
-    investmentInfo = row
-    #investmentInfo = investmentInfo.rename("bla")
-    #print(investmentInfo.name)
-    #print("FMV = ", investmentInfo.FMV)
-    #print(row.Region)
-    fund = portfolio.Fund(investmentInfo, [])
-    funds.update({fund.name : fund})
-    #print(fund.investmentinfo)
-columns
-print(funds["Fund3"])
-#funds["Fund3"].investmentinfoFMV
 
-# build portfolio
-pf = portfolio.Portfolio2("my Portfolio")
-pf.addFund(funds["Fund0"])
-pf.addFund(funds["Fund1"])
-pf.addFund(funds["Fund2"])
-
-#print(pf)
-#print(pf.funds)
-#print(funds["Fund0"])
-#pf.getPortfolio()
-#print(pf.funds["Fund0"])
-print("+++++++++++")
-print(funds['Fund0'].investmentinfo.values)
-#print(pf)
-#for key, value in funds.items():
-#    print(key+": "+str(value))
-
-
-print("+++++++++++")
-print(pf.getPortfolio())
-print(pf.getPortfolio().loc[0])
-
-#pf.getPortfolio().query('Fund == Fund2')
-
-#print(pf.getFunds()["Fund0"].investmentinfo.values)
-#print(funds)
-#pd.DataFrame.from_dict(funds, orient='index', columns=["ID","Fund","Vintage","Region","Strategy","CCY","FMV"])
-
-print("-----------")
-print(pf)
-print("-----------")
-#pf.getPortfolio()[pf.getPortfolio().Fund == "Fund2"]
-#pf.getPortfolio().loc[pf.getPortfolio()['Fund'] == 'Fund2']
 
 # <codecell>
 
-# build portfolio object
-pf = portfolio.Portfolio("my Portfolio",df_pf, df_data)
 
-# print portfolio information
-print(pf)
-print("-------------------")
-pf.getFund("Fund2")
 
 # <codecell>
 
-df_data.tail()
+
 
 # <codecell>
 
-# for cross reference: get reference date,
-# compute the age of the funds and add it (age)
-# to the portfolio dataframe:
-filename = "../data/portfolio.csv"
-refDate = mt.getReferenceDate(filename)
-refYear = mt.getReferenceYear(refDate)
 
-if (not 'Age' in df_pf.columns):
-    df_pf.insert(loc=3, column='Age',
-                 value=refYear - df_pf['Vintage'].values)
 
-# vintage in the data dataframe
-if (not 'Vintage' in df_data.columns):
-    df_data.insert(loc=2, column='Vintage',
-                   value=refYear-df_data['Age'])
+# <codecell>
+
+
 
 # <codecell>
 
 # pivot the dataframe:
-df_roi = pd.pivot_table(df_data, index=['Age', 'Vintage'],
+#df_roi = pd.pivot_table(pf.getPfRoiData(), index=['Age', 'Strategy'],
+df_roi = pd.pivot_table(df_data, index=['Age'],
                      columns=['Strategy'],
                      values=['ROI'],
                      aggfunc={'ROI': 
-                              [np.mean, np.std, np.min, np.max, np.var]},
+                              [np.mean, np.std, np.min, np.max, np.var, SharpeRatio]},
+                              #[np.mean, np.std, np.min, np.max, np.var]},
                               #[np.mean, np.var]},
                      fill_value = 0
                     )
@@ -222,10 +163,6 @@ all_means = list(np.array([buyout_mean, vc_mean]).flatten())
 mt.changeBarWidth(ax,.4)
 mt.addValToBarPlot(ax, all_means, xshift=0.075, precision=2)
 pylab.show()
-# saving figure in subfolder images
-imgname = "exercise1-barplot-avROIs-age-bothStrats.pdf"
-fig.savefig(os.path.join('images', imgname),
-            bbox_inches='tight', pad_inches=0.1)
 
 # <codecell>
 
@@ -258,35 +195,6 @@ print("However, the maximum ROI of "
      +str(round(df_data.max()['ROI'],5))
      +" is obtained by the strategy "+str(df_data.max()['Strategy'])
      +" with an age of 6 (Vintage: 2011)")
-
-# <codecell>
-
-# average ROI per vintage
-df_vintage = pd.pivot_table(df_data, index=['Vintage'],
-                     values=['ROI'],
-                     aggfunc={'ROI': 
-                              [np.mean, np.std, np.min, np.max, np.var]},
-                              #[np.mean, np.var]},
-                     fill_value = 0
-                    )
-df_vintage
-
-# <codecell>
-
-# bar plot in seaborn:
-fig=pylab.figure()
-vintages = df_vintage.index.values
-ax = sns.barplot(x="Vintage", y="ROI", ci=False, data=df_data)
-pylab.xlabel('Vintage')
-pylab.ylabel('Average ROI')
-pylab.title('Average ROIs plotted over vintages')
-mt.addValToBarPlot(ax, df_vintage['ROI']['mean'].values,
-                   xshift=0.15, yshift=-.1)
-pylab.show()
-# saving figure in subfolder images
-imgname = "exercise1-barplot-avROIs-accumulatedStrats.pdf"
-fig.savefig(os.path.join('images', imgname),
-            bbox_inches='tight', pad_inches=0.1)
 
 # <codecell>
 
