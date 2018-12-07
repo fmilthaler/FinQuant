a1177c07ad0307c1b2b65d222c59b9aaf30d291b
Frank Milthaler <f.milthaler@gmail.com>
2018-12-07 19:35:51 +0000

    Adding function to automatically extract relevant data (roi data) from the raw data that was given, based on the entries of the given portfolio

--- src/portfolio.py
+++ src/portfolio.py
@@ -15,9 +15,23 @@ class Portfolio(object):
             idx = self.portfolio.columns.get_loc('Year')
             self.portfolio.insert(loc=idx+1, column='Age',
                                   value=self.getRefYear()-self.portfolio['Year'].values)
+
     def extractPfRoiData(self):
-        pf_roi_data = None
-        return pd.DataFrame([])
+        # get dictionary that holds relevant information to extract 
+        # data from the raw dataset:
+        d = {}
+        for i in range(len(self.portfolio.index)):
+            d.update({self.portfolio.Name[i] : {'Age' : self.portfolio.Age[i],
+                                                'Strategy' : self.portfolio.Strategy[i]}})
+        # generate a string to query the relevant data:
+        querystring = ''
+        for key, value in d.items():
+            querystring = querystring+'Age=='+str(value['Age'])+' & Strategy=="'+str(value['Strategy'])+'" | '
+        # remove trailing ' | ' from querystring
+        querystring = querystring[:-3]
+        # get data for the given portfolio:
+        pf_roi_data = self.getRawData().query(querystring).reset_index(drop=True)
+        return pf_roi_data
 
     def getRefYear(self):
         return self.ref_year
@@ -25,7 +39,7 @@ class Portfolio(object):
         return self.portfolio
     def getRawData(self):
         return self.raw_data
-    def getPortfolioRoiData(self):
+    def getPfRoiData(self):
         return self.pf_roi_data
     def getFund(self, name):
         fund = self.getPortfolio().loc[self.getPortfolio()['Name'] == name]
