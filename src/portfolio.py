efb1a80f873e6852114a8e8a086f8889ada90278
Frank Milthaler <f.milthaler@gmail.com>
2018-12-08 23:25:02 +0000

    Cleaning up class Portfolio

--- src/portfolio.py
+++ src/portfolio.py
@@ -40,66 +40,19 @@ class Portfolio(object):
     of the portfolio investment information. The corresponding daily return of investments
     (ROI) are stored in the Fund object.
     '''
-    #def __init__(self, name, ref_year, portfolio):
     def __init__(self, name, ref_year):
+        # initilisating instance variables
         self.name = name
         self.ref_year = ref_year
+        # initialise some more instance variables that do not have a value yet
         self.portfolio = pd.DataFrame()
-        #self.portfolio = pd.DataFrame(portfolio)
         self.funds = {}
-        # add column "Age" to dataframe:
-        #self.addAge()
-        #self.pf_roi_data = self.extractPfRoiData()
-        #self.funds = self.extractFunds()
         self.pf_roi_data = pd.DataFrame()
-        # initialise other instance variables:
         self.pf_means = None
         self.pf_weights = None
         self.expectedRoi = None
         self.volatility = None
         self.covPf = None
-
-
-    #def addAge(self):
-    #    if (not 'Age' in self.portfolio.columns):
-    #        idx = self.portfolio.columns.get_loc('Year')
-    #        self.portfolio.insert(loc=idx+1, column='Age',
-    #                              value=self.getRefYear()-self.portfolio['Year'].values)
-
-    #def extractPfRoiData(self):
-    #    # get dictionary that holds relevant information to extract 
-    #    # data from the raw dataset:
-    #    d = {}
-    #    for i in range(len(self.portfolio.index)):
-    #        d.update({self.portfolio.Name[i] : {'Age' : self.portfolio.Age[i],
-    #                                            'Strategy' : self.portfolio.Strategy[i]}})
-    #    # generate a string to query the relevant data:
-    #    querystring = ''
-    #    for key, value in d.items():
-    #        querystring = querystring+'Age=='+str(value['Age'])+' & Strategy=="'+str(value['Strategy'])+'" | '
-    #    # remove trailing ' | ' from querystring
-    #    querystring = querystring[:-3]
-    #    # get data for the given portfolio:
-    #    pf_roi_data = self.getRawData().query(querystring).reset_index(drop=True)
-    #    return pf_roi_data
-
-    #def extractFunds(self):
-    #    funds = {}
-    #    for i in range(len(self.portfolio.index)):
-    #        name = self.portfolio.Name[i]
-    #        age = self.portfolio.Age[i]
-    #        strategy = self.portfolio.Strategy[i]
-    #        if ('Name' in self.pf_roi_data.columns):
-    #            querystring = 'Name=='+str(name)
-    #        elif (all(x in self.pf_roi_data.columns for x in ['Age', 'Strategy'])):
-    #            None
-    #        #elif ('Age' in self.pf_roi_data.columns):
-    #        #roi_data = 
-    #        roi_data = []
-    #        funds.update({name : {'Age' : age,
-    #                              'Strategy' : strategy,
-    #                              'ROI' : roi_data}})
-    #    return funds
 
     def addFund(self, fund):
         # adding fund to dictionary containing all funds provided
@@ -118,9 +71,6 @@ class Portfolio(object):
     # get functions:
     def getPortfolio(self):
         return self.portfolio
-
-    def getFunds(self):
-        return self.funds
 
     def getRefYear(self):
         return self.ref_year
@@ -178,10 +128,6 @@ class Portfolio(object):
         # in respect of the total investment
         total = self.portfolio.FMV.sum()
         pf_weights = self.portfolio.FMV/total
-        #weights = []
-        #for key in self.funds.keys():
-        #    weights.append(self.getFund(key).getInvestmentInfo().FMV/total)
-        #return np.array(weights)
         # set instance variable
         self.setPfWeights(pf_weights)
         return pf_weights
