7aa8423bb7e92ef72d74b34c3c5d1d01d0c314e8
Frank Milthaler <f.milthaler@gmail.com>
2019-01-02 18:24:32 +0000

    Flake8 corrections

--- qpy/quanttools.py
+++ qpy/quanttools.py
@@ -1,10 +1,13 @@
 import numpy as np
 
+
 def weightedMean(means, weights):
     return np.sum(means * weights)
 
+
 def weightedStd(cov_matrix, weights):
     return np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))
+
 
 def SharpeRatio(exproi, riskfreerate, volatility):
     return (exproi - riskfreerate)/float(volatility)
