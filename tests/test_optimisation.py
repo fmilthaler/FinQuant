1f4027af684527f7097b4c67cdcf8303e297ce48
Frank Milthaler <f.milthaler@gmail.com>
2019-01-25 19:38:02 +0000

    Adding empty test files for optimisation/efficient_frontier. For now these are empty, as their functionality is tested through portfolio

---
+++ tests/test_optimisation.py
@@ -0,0 +1,4 @@
+###################################################
+# for now, the corresponding module in quantpy is #
+# tested through portfolio                        #
+###################################################
