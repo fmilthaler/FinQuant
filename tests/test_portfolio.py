a83722829325f55acb4103173f060edf0f29d2c3
Frank Milthaler <f.milthaler@gmail.com>
2023-07-05 18:20:17 +0200

    re-adding tests for MC optimisation, EF optimisation, and plots

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -243,182 +243,228 @@ def test_expected_fails():
             build_portfolio(**fail_test_settings)
 
 
-# ######################################
-# # tests for Monte Carlo optimisation #
-# ######################################
-#
-#
-# def test_mc_optimisation():
-#     d = d_pass[3]
-#     pf = build_portfolio(**d)
-#     # since the monte carlo optimisation is based on random numbers,
-#     # we set a seed, so that the results can be compared.
-#     np.random.seed(seed=0)
-#     # orig values:
-#     minvol_res_orig = [0.18560926749041448, 0.1333176229402258, 1.3547291311321408]
-#     maxsharpe_res_orig = [0.33033770744503416, 0.16741461860370618, 1.9433052511092475]
-#     minvol_w_orig = [
-#         0.09024151309669741,
-#         0.015766238378839476,
-#         0.514540537132381,
-#         0.37945171139208195,
-#     ]
-#     maxsharpe_w_orig = [
-#         0.018038812127367274,
-#         0.37348740385059126,
-#         0.5759648343129179,
-#         0.03250894970912355,
-#     ]
-#     labels_orig = ["min Volatility", "max Sharpe Ratio", "Initial Portfolio"]
-#     xlabel_orig = "Volatility [period=252]"
-#     ylabel_orig = "Expected Return [period=252]"
-#     # run Monte Carlo optimisation through pf
-#     opt_w, opt_res = pf.mc_optimisation(num_trials=500)
-#     # tests
-#     assert (minvol_res_orig - opt_res.iloc[0].values <= strong_abse).all()
-#     assert (maxsharpe_res_orig - opt_res.iloc[1].values <= strong_abse).all()
-#     assert (minvol_w_orig - opt_w.iloc[0].values <= strong_abse).all()
-#     assert (maxsharpe_w_orig - opt_w.iloc[1].values <= strong_abse).all()
-#     # also test the plot
-#     plt.figure()
-#     pf.mc_plot_results()
-#     # get axis object
-#     ax = plt.gca()
-#     # only checking legend and axis labels, and assume that the plot
-#     # was successfully created
-#     labels_plot = ax.get_legend_handles_labels()[1]
-#     xlabel_plot = ax.get_xlabel()
-#     ylabel_plot = ax.get_ylabel()
-#     assert labels_orig == labels_plot
-#     assert xlabel_orig == xlabel_plot
-#     assert ylabel_orig == ylabel_plot
-#
-#
-# #############################################
-# # tests for Efficient Frontier optimisation #
-# #############################################
-#
-#
-# def test_get_ef():
-#     d = d_pass[3]
-#     pf = build_portfolio(**d)
-#     ef = pf._get_ef()
-#     assert isinstance(ef, EfficientFrontier)
-#     assert isinstance(pf.ef, EfficientFrontier)
-#     assert pf.ef == ef
-#     assert (pf.comp_mean_returns(freq=1) == ef.mean_returns).all()
-#     assert (pf.comp_cov() == ef.cov_matrix).all().all()
-#     assert pf.freq == ef.freq
-#     assert pf.risk_free_rate == ef.risk_free_rate
-#     assert ef.names == pf.portfolio["Name"].values.tolist()
-#     assert ef.num_stocks == len(pf.stocks)
-#
-#
-# def test_ef_minimum_volatility():
-#     d = d_pass[3]
-#     pf = build_portfolio(**d)
-#     min_vol_weights = np.array(
-#         [
-#             0.15515521225480033,
-#             2.168404344971009e-18,
-#             0.4946241856546514,
-#             0.35022060209054834,
-#         ]
-#     )
-#     ef_opt_weights = pf.ef_minimum_volatility()
-#     assert np.allclose(
-#         ef_opt_weights.values.transpose(), min_vol_weights, atol=weak_abse
-#     )
-#
-#
-# def test_maximum_sharpe_ratio():
-#     d = d_pass[3]
-#     pf = build_portfolio(**d)
-#     max_sharpe_weights = np.array(
-#         [0.0, 0.41322217986076903, 0.5867778201392311, 2.2858514942065993e-17]
-#     )
-#     ef_opt_weights = pf.ef_maximum_sharpe_ratio()
-#     assert np.allclose(
-#         ef_opt_weights.values.transpose(), max_sharpe_weights, atol=weak_abse
-#     )
-#
-#
-# def test_efficient_return():
-#     d = d_pass[3]
-#     pf = build_portfolio(**d)
-#     efficient_return_weights = np.array(
-#         [
-#             0.09339785373366818,
-#             0.1610699937778475,
-#             0.5623652130240258,
-#             0.18316693946445858,
-#         ]
-#     )
-#     ef_opt_weights = pf.ef_efficient_return(0.2542)
-#     assert np.allclose(
-#         ef_opt_weights.values.transpose(), efficient_return_weights, atol=weak_abse
-#     )
-#
-#
-# def test_efficient_volatility():
-#     d = d_pass[3]
-#     pf = build_portfolio(**d)
-#     efficient_volatility_weights = np.array(
-#         [0.0, 0.5325563159992046, 0.4674436840007955, 0.0]
-#     )
-#     ef_opt_weights = pf.ef_efficient_volatility(0.191)
-#     assert np.allclose(
-#         ef_opt_weights.values.transpose(), efficient_volatility_weights, atol=weak_abse
-#     )
-#
-#
-# def test_efficient_frontier():
-#     d = d_pass[3]
-#     pf = build_portfolio(**d)
-#     efrontier = np.array(
-#         [
-#             [0.13309804281267365, 0.2],
-#             [0.13382500317474913, 0.21],
-#             [0.13491049715255884, 0.22],
-#             [0.13634357140158387, 0.23],
-#             [0.13811756026939967, 0.24],
-#             [0.14021770106367654, 0.25],
-#             [0.1426296319926618, 0.26],
-#             [0.1453376889002686, 0.27],
-#             [0.14832574432346657, 0.28],
-#             [0.15157724434785497, 0.29],
-#             [0.15507572162309227, 0.3],
-#         ]
-#     )
-#     targets = [round(0.2 + 0.01 * i, 2) for i in range(11)]
-#     ef_efrontier = pf.ef_efficient_frontier(targets)
-#     assert np.allclose(ef_efrontier, efrontier, atol=weak_abse)
-#
-#
-# ########################################################
-# # tests for some portfolio/efficient frontier plotting #
-# # only checking for errors/exceptions that pop up      #
-# ########################################################
-#
-#
-# def test_plot_efrontier():
-#     d = d_pass[3]
-#     pf = build_portfolio(**d)
-#     # just checking if a plot is successfully created,
-#     # not checking for content
-#     pf.ef_plot_efrontier()
-#
-#
-# def test_plot_optimal_portfolios():
-#     d = d_pass[3]
-#     pf = build_portfolio(**d)
-#     # just checking if a plot is successfully created,
-#     # not checking for content
-#     pf.ef_plot_optimal_portfolios()
-#
-#
-# def test_plot_stocks():
-#     d = d_pass[3]
-#     pf = build_portfolio(**d)
-#     # just checking if a plot is successfully created, not checking for content
-#     pf.plot_stocks()
+######################################
+# tests for Monte Carlo optimisation #
+######################################
+
+
+def test_mc_optimisation():
+    d = d_pass[4]
+    pf = build_portfolio(**d)
+    # since the monte carlo optimisation is based on random numbers,
+    # we set a seed, so that the results can be compared.
+    np.random.seed(seed=0)
+    # orig values:
+    minvol_res_orig = [0.18560926749041448, 0.1333176229402258, 1.3547291311321408]
+    maxsharpe_res_orig = [0.33033770744503416, 0.16741461860370618, 1.9433052511092475]
+    minvol_w_orig = [
+        0.09024151309669741,
+        0.015766238378839476,
+        0.514540537132381,
+        0.37945171139208195,
+    ]
+    maxsharpe_w_orig = [
+        0.018038812127367274,
+        0.37348740385059126,
+        0.5759648343129179,
+        0.03250894970912355,
+    ]
+    labels_orig = ["min Volatility", "max Sharpe Ratio", "Initial Portfolio"]
+    xlabel_orig = "Volatility [period=252]"
+    ylabel_orig = "Expected Return [period=252]"
+    # run Monte Carlo optimisation through pf
+    opt_w, opt_res = pf.mc_optimisation(num_trials=500)
+    # tests
+    assert (minvol_res_orig - opt_res.iloc[0].values <= strong_abse).all()
+    assert (maxsharpe_res_orig - opt_res.iloc[1].values <= strong_abse).all()
+    assert (minvol_w_orig - opt_w.iloc[0].values <= strong_abse).all()
+    assert (maxsharpe_w_orig - opt_w.iloc[1].values <= strong_abse).all()
+
+
+#############################################
+# tests for Efficient Frontier optimisation #
+#############################################
+
+
+def test_get_ef():
+    d = d_pass[4]
+    pf = build_portfolio(**d)
+    ef = pf._get_ef()
+    assert isinstance(ef, EfficientFrontier)
+    assert isinstance(pf.ef, EfficientFrontier)
+    assert pf.ef == ef
+    assert (pf.comp_mean_returns(freq=1) == ef.mean_returns).all()
+    assert (pf.comp_cov() == ef.cov_matrix).all().all()
+    assert pf.freq == ef.freq
+    assert pf.risk_free_rate == ef.risk_free_rate
+    assert ef.names == pf.portfolio["Name"].values.tolist()
+    assert ef.num_stocks == len(pf.stocks)
+
+
+def test_ef_minimum_volatility():
+    d = d_pass[4]
+    pf = build_portfolio(**d)
+    min_vol_weights = np.array(
+        [
+            0.15515521225480033,
+            2.168404344971009e-18,
+            0.4946241856546514,
+            0.35022060209054834,
+        ]
+    )
+    ef_opt_weights = pf.ef_minimum_volatility()
+    assert np.allclose(
+        ef_opt_weights.values.transpose(), min_vol_weights, atol=weak_abse
+    )
+
+
+def test_maximum_sharpe_ratio():
+    d = d_pass[4]
+    pf = build_portfolio(**d)
+    max_sharpe_weights = np.array(
+        [0.0, 0.41322217986076903, 0.5867778201392311, 2.2858514942065993e-17]
+    )
+    ef_opt_weights = pf.ef_maximum_sharpe_ratio()
+    assert np.allclose(
+        ef_opt_weights.values.transpose(), max_sharpe_weights, atol=weak_abse
+    )
+
+
+def test_efficient_return():
+    d = d_pass[4]
+    pf = build_portfolio(**d)
+    efficient_return_weights = np.array(
+        [
+            0.09339785373366818,
+            0.1610699937778475,
+            0.5623652130240258,
+            0.18316693946445858,
+        ]
+    )
+    ef_opt_weights = pf.ef_efficient_return(0.2542)
+    assert np.allclose(
+        ef_opt_weights.values.transpose(), efficient_return_weights, atol=weak_abse
+    )
+
+
+def test_efficient_volatility():
+    d = d_pass[4]
+    pf = build_portfolio(**d)
+    efficient_volatility_weights = np.array(
+        [0.0, 0.5325563159992046, 0.4674436840007955, 0.0]
+    )
+    ef_opt_weights = pf.ef_efficient_volatility(0.191)
+    assert np.allclose(
+        ef_opt_weights.values.transpose(), efficient_volatility_weights, atol=weak_abse
+    )
+
+
+def test_efficient_frontier():
+    d = d_pass[4]
+    pf = build_portfolio(**d)
+    efrontier = np.array(
+        [
+            [0.13309804281267365, 0.2],
+            [0.13382500317474913, 0.21],
+            [0.13491049715255884, 0.22],
+            [0.13634357140158387, 0.23],
+            [0.13811756026939967, 0.24],
+            [0.14021770106367654, 0.25],
+            [0.1426296319926618, 0.26],
+            [0.1453376889002686, 0.27],
+            [0.14832574432346657, 0.28],
+            [0.15157724434785497, 0.29],
+            [0.15507572162309227, 0.3],
+        ]
+    )
+    targets = [round(0.2 + 0.01 * i, 2) for i in range(11)]
+    ef_efrontier = pf.ef_efficient_frontier(targets)
+    assert np.allclose(ef_efrontier, efrontier, atol=weak_abse)
+
+
+########################################################
+# tests for some portfolio/efficient frontier plotting #
+# only checking for errors/exceptions that pop up      #
+########################################################
+plt.switch_backend("Agg")
+
+
+def test_plot_efrontier():
+    d = d_pass[4]
+    pf = build_portfolio(**d)
+    # Clear the current figure to ensure a fresh plot
+    plt.clf()
+    # Create the plot
+    pf.ef_plot_efrontier()
+    # Assert that the plot was created
+    assert len(plt.gcf().get_axes()) == 1
+
+    # get title, axis labels and axis min/max values
+    ax = plt.gca()
+    title = ax.get_title()
+    xlabel = ax.get_xlabel()
+    ylabel = ax.get_ylabel()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    # expected values:
+    expected_title = "Efficient Frontier"
+    expected_xlable = "Volatility"
+    expected_ylable = "Expected Return"
+    expected_xlim = (0.12504, 0.29359)
+    expected_ylim = (0.05445, 0.50655)
+
+    # assert on title, labels and limits
+    assert title == expected_title
+    assert xlabel == expected_xlable
+    assert ylabel == expected_ylable
+    assert xlim == pytest.approx(expected_xlim, 1e-3)
+    assert ylim == pytest.approx(expected_ylim, 1e-3)
+
+
+def test_plot_optimal_portfolios():
+    d = d_pass[4]
+    pf = build_portfolio(**d)
+    # Clear the current figure to ensure a fresh plot
+    plt.clf()
+    # Create the plot
+    pf.ef_plot_optimal_portfolios()
+    # Assert that the plot was created
+    assert len(plt.gcf().get_axes()) == 1
+
+    # get axis min/max values
+    ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    # expected values:
+    expected_xlim = (0.13064, 0.17607)
+    expected_ylim = (0.17957, 0.35317)
+
+    # assert on title, labels and limits
+    assert xlim == pytest.approx(expected_xlim, 1e-3)
+    assert ylim == pytest.approx(expected_ylim, 1e-3)
+
+
+def test_plot_stocks():
+    d = d_pass[4]
+    pf = build_portfolio(**d)
+    # Clear the current figure to ensure a fresh plot
+    plt.clf()
+    # Create the plot
+    pf.plot_stocks()
+    # Assert that the plot was created
+    assert len(plt.gcf().get_axes()) == 1
+
+    # get axis min/max values
+    ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    # expected values:
+    expected_xlim = (0.15426, 0.29255)
+    expected_ylim = (0.05403, 0.50693)
+
+    # assert on title, labels and limits
+    assert xlim == pytest.approx(expected_xlim, 1e-3)
+    assert ylim == pytest.approx(expected_ylim, 1e-3)
