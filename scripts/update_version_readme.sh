bfe1d2bc1095d436218dee35f4edd79925ed672d
Frank Milthaler <f.milthaler@gmail.com>
2023-07-22 20:28:31 +0200

    merging both scripts to update readmes

--- scripts/update_version_readme.sh
+++ scripts/update_version_readme.sh
@@ -2,7 +2,7 @@
 
 update_version_readme() {
 	local version_file="version"
-	local readme_md="README.md"
+	local readme_md="$1"
 
 	# Read the current version from the "version" file
 	local current_version=$(grep -Eo 'version=([0-9]+\.){2}[0-9]+' "$version_file" | cut -d'=' -f2)
@@ -17,5 +17,26 @@ update_version_readme() {
 	update_file "$readme_md"
 }
 
-# Call the update_version function
-update_version_readme
+update_readme_tex() {
+	local file_path="$1"
+
+	# Copy README.md to README.tex.md
+	cp README.md "$file_path"
+
+	# Read the contents of README.tex.md
+	local content=$(<"$file_path")
+
+	# Replace patterns
+	content=$(echo "$content" | sed -E "s/<img src=\"(.*?)\" align=middle width=194.52263655pt height=46.976899200000005pt\/>/\$\\\\displaystyle\\\\dfrac{\\\\text{price}_{t_i} - \\\\text{price}_{t_0} + \\\\text{dividend}}{\\\\text{price}_{t_0}}\$/")
+
+	content=$(echo "$content" | sed -E "s/<img src=\"(.*?)\" align=middle width=126.07712039999997pt height=48.84266309999997pt\/>/\$\\\\displaystyle\\\\dfrac{\\\\text{price}_{t_i} - \\\\text{price}_{t_{i-1}}}{\\\\text{price}_{t_{i-1}}}\$/")
+
+	content=$(echo "$content" | sed -E "s/<img src=\"(.*?)\" align=middle width=208.3327686pt height=57.53473439999999pt\/>/\$\\\\displaystyle\\\\log\\\\left(1 + \\\\dfrac{\\\\text{price}_{t_i} - \\\\text{price}_{t_{i-1}}}{\\\\text{price}_{t_{i-1}}}\\\\right)\$/")
+
+	# Write the updated contents back to README.tex.md
+	echo "$content" > "$file_path"
+}
+
+# Update both readme files:
+update_version_readme "README.md"
+update_readme_tex "README.tex.md"
