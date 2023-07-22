3ca21a8d1021b25d9912d499c4bd40cfdc8b0832
Frank Milthaler <f.milthaler@gmail.com>
2023-07-22 20:52:05 +0200

    fixing workflow and script

--- scripts/auto-commit.sh
+++ scripts/auto-commit.sh
@@ -9,19 +9,17 @@ echo "COMMITMSG: $COMMITMSG"
 
 # Stage changes
 #git add $(git ls-files)
-git add --udpate
+git add --update
 
 # Check Git diff-index
 git diff-index --quiet HEAD --
 
 if [ $? -eq 0 ]; then
 	echo "No changes found, nothing to see/do here."
+	exit 1
 else
 	echo "Changes found. Preparing commit."
 	git config --local user.email "github-actions[bot]@users.noreply.github.com"
 	git config --local user.name "github-actions[bot]"
 	git commit -m "${COMMITMSG}"
 fi
-
-git log | head
-exit 0
