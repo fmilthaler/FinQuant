9d9276b775e19d750553e3fcdb933b4e42bb386b
Frank Milthaler <f.milthaler@gmail.com>
2023-07-22 20:37:10 +0200

    adding new script to commit changes (if present), and removing that code from other scripts

---
+++ scripts/auto-format.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Code formatting with isort and black
+echo "Code formatting with isort and black:"
+isort $(git ls-files '*.py')
+black $(git ls-files '*.py')
+
+## Stage changes
+##git add $(git ls-files)
+#git add --udpate
+#
+## Check Git diff-index
+#git diff-index --quiet HEAD --
+#
+#if [ $? -eq 0 ]; then
+#	echo "No changes found, nothing to see/do here."
+#else
+#	echo "Changes found. Preparing commit."
+#	git config --local user.email "github-actions[bot]@users.noreply.github.com"
+#	git config --local user.name "github-actions[bot]"
+#	git commit -m "Automated formatting changes"
+#fi
+#
+#git log | head
+#exit 0
