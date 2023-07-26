715265133ea0c8d1374528cf12381dfef082ad82
Frank Milthaler <f.milthaler@gmail.com>
2023-07-26 11:46:09 +0200

    adding unformatted code

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -5,6 +5,8 @@
 import datetime
 import os
 import pathlib
+from finquant.portfolio import build_portfolio
+
 
 import numpy as np
 import pandas as pd
@@ -66,3 +68,17 @@ def test_Stock():
         assert all(
             stock.investmentinfo == pf.portfolio.loc[pf.portfolio["Name"] == stock.name]
         )
+
+
+
+
+
+def hello_world(s:    str)    -> None:
+
+
+
+    print("hi")
+
+
+
+    return None
