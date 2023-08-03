12a08fbf8748133eabc2eeb266abdacd2788617a
Frank Milthaler <f.milthaler@gmail.com>
2023-08-03 21:04:13 +0200

    adjusting type_validation for quants functions

--- finquant/type_utilities.py
+++ finquant/type_utilities.py
@@ -36,17 +36,17 @@ def type_validation(**kwargs: Any) -> None:
 
     Example usage:
         type_validation(
-        data=pd.DataFrame(),
+        data=pd.DataFrame([1., 2.]),
         names=["name1", "name2"],
         start_date="2023-08-01",
-        freq=10,
-        generic_df=[df1, df2],
+        freq=10.0,
     )
     """
 
     dtype_msg: str = " with dtype 'np.float64'"
 
     type_dict = {
+        # DataFrames, Series, Arrays:
         "data": (pd.DataFrame, "a non-empty pandas.DataFrame"),
         "pf_allocation": (pd.DataFrame, "a non-empty pd.DataFrame"),
         "means": (
@@ -61,37 +61,39 @@ def type_validation(**kwargs: Any) -> None:
             Union[np.ndarray, pd.DataFrame],
             f"a non-empty numpy.ndarray or pandas.DataFrame {dtype_msg}.",
         ),
+        # Lists:
         "names": (
             Union[List, np.ndarray],
             "a non-empty List[str] or numpy.ndarray[str]",
         ),
         "cols": (List, "a non-empty List[str]"),
+        # Datatime objects:
         "start_date": (
             Union[str, datetime.datetime],
             "of type str or datetime.datetime",
         ),
         "end_date": (Union[str, datetime.datetime], "of type str or datetime.datetime"),
+        # Strings:
         "data_api": (str, "of type str"),
         "market_index": (str, "of type str"),
+        # FLOATs
+        "expected_return": ((float, np.floating), "of type float"),
+        "volatility": ((float, np.floating), "of type float"),
+        "risk_free_rate": ((float, np.floating), "of type float"),
+        "downside_risk": ((float, np.floating), "of type float"),
+        "mu": ((float, np.floating), "of type float"),
+        "sigma": ((float, np.floating), "of type float"),
+        "conf_level": ((float, np.floating), "of type float"),
+        # INTs:
         "freq": ((int, np.integer), "of type integer"),
-        "generic_str": (str, "of type str"),
-        "generic_int": ((int, np.integer), "of type integer"),
-        "generic_float": ((float, np.floating), "of type float"),
-        "generic_numeric": (
-            (int, np.integer, float, np.floating),
-            "of type integer or float",
-        ),
-        "generic_df": (pd.DataFrame, "a non-empty pd.DataFrame"),
-        "generic_series": (pd.Series, "a non-empty pandas.Series"),
-        "generic_datetime": (
-            Union[str, datetime.datetime],
-            "of type str or datetime.datetime",
-        ),
+        # NUMERICs:
+        "investment": ((int, np.integer, float, np.floating), "of type integer of float"),
     }
 
     for arg_name, (arg_type, expected_type) in type_dict.items():
         arg_values = kwargs.get(arg_name)
         if arg_values is not None:
+
             # Validating List[str] types:
             if arg_name in ("names", "cols"):
                 if not isinstance(arg_values, arg_type) or (
@@ -108,11 +110,11 @@ def type_validation(**kwargs: Any) -> None:
                 continue
 
             # Validating common Array[FLOAT], Series[Float], DataFrame[FLOAT] types
-            if arg_name in ("data", "means", "weights", "cov_matrix"):
+            if arg_name in ("data", "pf_allocation", "means", "weights", "cov_matrix"):
                 if (
                     not isinstance(arg_values, arg_type)
                     or (
-                        isinstance(arg_values, Union[np.ndarray, pd.Series])
+                        isinstance(arg_values, (np.ndarray, pd.Series))
                         and not arg_values.dtype == np.float64
                     )
                     or (
@@ -129,19 +131,16 @@ def type_validation(**kwargs: Any) -> None:
                     )
                 continue
 
-            # else (arg_name is not "names" nor "cols")
-            if not isinstance(arg_values, (List, np.ndarray)):
-                arg_values = [arg_values]
-            for arg_value in arg_values:
-                if not isinstance(arg_value, arg_type):
-                    raise TypeError(
-                        f"Error: {arg_name} is expected to be {expected_type}."
-                    )
-                if (
-                    isinstance(arg_value, (pd.Series, pd.DataFrame)) and arg_value.empty
-                ) or (
-                    isinstance(arg_value, (List, np.ndarray)) and len(arg_value) == 0
-                ):
-                    raise ValueError(
-                        f"Error: {arg_name} is expected to be non-empty {expected_type}."
-                    )
+            # Other types (str, numeric):
+            if not isinstance(arg_values, arg_type):
+                raise TypeError(
+                    f"Error: {arg_name} is expected to be {expected_type}."
+                )
+            if (
+                isinstance(arg_values, (pd.Series, pd.DataFrame)) and arg_values.empty
+            ) or (
+                isinstance(arg_values, (List, np.ndarray)) and len(arg_values) == 0
+            ):
+                raise ValueError(
+                    f"Error: {arg_name} is expected to be non-empty {expected_type}."
+                )
